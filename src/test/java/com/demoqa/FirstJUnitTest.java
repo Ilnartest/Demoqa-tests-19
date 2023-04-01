b2a06d2a39e2c1f430f4f8b9ad51d82735055fed
Ilnartest <kadyyr@yandex.ru>
2023-04-01 20:40:25 +0300

    Example JUnit

---
+++ src/test/java/com/demoqa/FirstJUnitTest.java
@@ -0,0 +1,35 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+public class FirstJUnitTest {
+    @BeforeAll
+    static void BeforeAll() {
+        System.out.println("    ###@BeforeAll");
+    }
+    @AfterAll
+    static void AfterAll() {
+        System.out.println("    ###@AfterAll");
+    }
+
+    @BeforeEach
+    void BeforeEach() {
+        System.out.println("    ###@BeforeEach!");
+    }
+    @AfterEach
+    void AfterEach() {
+        System.out.println("    ###@AfterEach!");
+    }
+
+    @Test
+   void FirstTest() {
+        Assertions.assertTrue(3>2);
+        System.out.println("    ###@FirstTest!");
+    }
+
+    @Test
+    void SecondTest() {
+        Assertions.assertTrue(3>2);
+        System.out.println("    ###@SecondTest!");
+    }
+}
