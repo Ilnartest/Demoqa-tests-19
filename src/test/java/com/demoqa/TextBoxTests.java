a6c33d679cfb27e75fea4118cb63cd478e1a731a
Ilnartest <kadyyr@yandex.ru>
2023-04-01 21:54:01 +0300

    TextBOX

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,27 @@
+package com.demoqa;
+
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests extends TestBase {
+
+    @Test
+    void successfulFillFormTest() {
+        open("/text-box");
+
+        $("#userName").setValue("VOVA");
+        $("#userEmail").setValue("VOVA@PUTIN.RU");
+        $("#currentAddress").setValue("str.Moskow-1");
+        $("#permanentAddress").setValue("str.Kremlin-1");
+        $("#submit").click();
+
+
+        $("#output").should(text("VOVA"),text("VOVA@PUTIN.RU"),
+                text("str.Moskow-1"),text("str.Kremlin-1"));
+    }
+}
