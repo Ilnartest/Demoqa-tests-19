75065a3472f44104c26ea9c1c6d1978ebfdfdc17
Ilnartest <kadyyr@yandex.ru>
2023-04-02 19:59:37 +0300

    Fixed StudentRegistrationForm

--- src/test/java/com/demoqa/StudentRegistrationForm.java
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -17,13 +17,13 @@ public class StudentRegistrationForm extends TestBase {
         $("#firstName").setValue("Ilnar");
         $("#lastName").setValue("Kadyyrov");
         $("#userEmail").setValue("kadyyr@ya.ru");
-        $("#gender-radio-1").parent().click();
-        $("#userNumber").setValue("89009999999");
+        $("#genterWrapper").$(byText("Male")).click();;
+        $("#userNumber").setValue("8900999999");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__year-select").selectOptionByValue("1987");
         $(".react-datepicker__month-select").selectOptionByValue("6");
         $(".react-datepicker__day--014").click();
-        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#hobbies-checkbox-1").parent().click();
         $("#uploadPicture").uploadFromClasspath("image.jpg");
         $("#currentAddress").setValue("str.Moscow-1");
@@ -36,6 +36,8 @@ public class StudentRegistrationForm extends TestBase {
 
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Ilnar"), text("Kadyyrov"),
+                text("kadyyr@ya.ru"), text("8900999999"));
 
     }
 }
