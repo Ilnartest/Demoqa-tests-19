0d567d2ec946b29d6733bf0135e451c99cc909f1
Ilnartest <kadyyr@yandex.ru>
2023-04-02 08:33:04 +0300

    StudentRegistrationForm

---
+++ src/test/java/com/demoqa/StudentRegistrationForm.java
@@ -0,0 +1,46 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class StudentRegistrationForm extends TestBase {
+    @Test
+    void successfulFillFormTest() {
+        open("/automation-practice-form");
+        executeJavaScript ("$('#fixedban').remove()");
+        executeJavaScript ("$ ('footer').remove ()");
+
+        $("#firstName").setValue("Ilnar");
+        $("#lastName").setValue("Kadyyrov");
+        $("#userEmail").setValue("kadyyr@ya.ru");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue("89009999999");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1987");
+        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__day--014").click();
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#uploadPicture").uploadFromClasspath("image.jpg");
+        $("#currentAddress").setValue("str.Moscow-1");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+
+    }
+}
+
